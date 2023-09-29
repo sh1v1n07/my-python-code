9790c2ea85ff5700aa531d662ef1808634dc64ef
sh1v1n07 <146410320+sh1v1n07@users.noreply.github.com>
2023-09-29 11:41:17 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+print(100)
+print("Hello World") 
+print("hey there!")
+print(23+100)
+x=100
+a=3,6
+print(x,z,a)
+#printing data type of variables
+print(type(z))
+#maths ops
+sum1=x+a
+nul1=x+a
+print(sum1,nul1)
+
---
+++ task1.py
@@ -0,0 +1,6 @@
+n1=input("enter the first number :")
+#by default input takes all arguments as string
+n2=input("enter the second number :")
+#change data type to int
+sum1=int(n1)+int(n2)
+print(sum1)
